e9f037af03ddf8b55c0602018d558997f21f1cab
wc <wchamberlain89@gmail.com>
2021-09-18 14:58:43 -0700

    Adds Product fetching to Home page.
    Adds getStaticProps to Home Page component to source products from the collection.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 import Head from 'next/head'
+import { getProductsInCollection } from '../lib/shopify'
 
-export default function Home() {
+export default function Home({ products }) {
+  console.log("Products on Home page are", products)
+
   return (
     <div className='text-3xl'>Hello from Shopify Next</div>
   )
+}
+
+export const getStaticProps = async () => {
+  const products = await getProductsInCollection()
+
+  return {
+    props: { products }
+  }
 }
